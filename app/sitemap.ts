6af4cd69f2323278238e34917ea4ef030d4507a4
M0mahmoud <houdmohamed85@gmail.com>
2024-08-06 02:51:09 +0300

    New Domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://devmahmoud.vercel.app",
+      url: "https://devmahmoud.me",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -12,17 +12,35 @@ const Hero = () => {
           <span className="hsp">FrontEnd Developer</span>
         </h1>
 
-        <p className="max-w-lg font-medium  text-gray-400">Front-End Developer From Egypt. I Have Rich Experience In Web Site Development And Building, I Also Work With Frame Work React Js, And I Love To Talk With You About Our Unique.</p>
+        <p className="max-w-lg font-medium  text-gray-400">
+          Front-End Developer From Egypt. I Have Rich Experience In Web Site
+          Development And Building, I Also Work With Frame Work React Js, And I
+          Love To Talk With You About Our Unique.
+        </p>
         <div className="flex justify-start items-center gap-5 flex-wrap">
-          <Link target="_blank" href={CV} className="border-white border-[1px] rounded-full  px-8 py-2 ">
+          <Link
+            target="_blank"
+            href={CV}
+            className="border-white border-[1px] rounded-full  px-8 py-2 "
+          >
             Open CV
           </Link>
-          <Link href={"#contact"} className="border-none  rounded-full  px-4 py-2 sp">
+          <Link
+            href={"#contact"}
+            className="border-none  rounded-full  px-4 py-2 sp"
+          >
             Let&#39;s Talk{" "}
           </Link>
         </div>
       </div>
-      <Image src={"/mahmoud-removebg.png"} alt="Mahmoud" width={350} height={380} className="bg-cover " />
+      <Image
+        src={"/mahmoud-removebg.png"}
+        alt="Mahmoud"
+        width={350}
+        height={380}
+        className="bg-cover"
+        loading="eager"
+      />
     </section>
   );
 };
