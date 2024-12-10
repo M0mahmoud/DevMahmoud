f97e12c340431cce4d2b1dc94546a6bb667e083a
M0mahmoud <houdmohamed85@gmail.com>
2024-12-10 23:52:30 +0200

    Optimize LCP

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,32 +1,36 @@
-import Image from "next/image";
 import Link from "next/link";
 
 const Nav = () => {
-  return (
-    <nav
-      id="home"
-      className="z-50 flex items-center justify-between w-full py-4"
-    >
-      <Link aria-label="My Logo" href={"/"} className="">
-        <Image
-          src="/MYLOGO.svg"
-          width={80}
-          height={80}
-          alt="Logo"
-          className="object-contain mr-auto"
-        />
-      </Link>
+    return (
+        <nav
+            id="home"
+            className="z-50 flex items-center justify-between w-full py-4"
+        >
+            <Link aria-label="My Logo" href={"/"} className="">
+                <img
+                    src="/MYLOGO.svg"
+                    width={80}
+                    height={80}
+                    alt="Logo"
+                    className="object-contain mr-auto"
+                    fetchPriority="high"
+                    rel=""
+                />
+            </Link>
 
-      <div className="flex items-center justify-center gap-4">
-        <Link href={"/blog"} className="text-xl text-white uppercase">
-          Blog
-        </Link>
-        <Link href={"#contact"} className="text-xl text-white uppercase">
-          Contact
-        </Link>
-      </div>
-    </nav>
-  );
+            <div className="flex items-center justify-center gap-4">
+                <Link href={"/blog"} className="text-xl text-white uppercase">
+                    Blog
+                </Link>
+                <Link
+                    href={"#contact"}
+                    className="text-xl text-white uppercase"
+                >
+                    Contact
+                </Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,48 +1,48 @@
 import { CV } from "@/constant";
-import Image from "next/image";
 import Link from "next/link";
 
 const Hero = () => {
-  return (
-    <section className="flex justify-between items-center flex-col sm:flex-row gap-6">
-      <div className="flex items-start justify-center flex-col gap-12">
-        <h1 className="text-4xl leading-10 font-bold">
-          Hi, I am Mahmoud
-          <br />
-          <span className="hsp">FrontEnd Developer</span>
-        </h1>
+    return (
+        <section className="flex justify-between items-center flex-col sm:flex-row gap-6">
+            <div className="flex items-start justify-center flex-col gap-12">
+                <h1 className="text-4xl leading-10 font-bold">
+                    Hi, I am Mahmoud
+                    <br />
+                    <span className="hsp">FrontEnd Developer</span>
+                </h1>
 
-        <p className="max-w-lg font-medium  text-gray-400">
-          Front-End Developer From Egypt. I Have Rich Experience In Web Site
-          Development And Building, I Also Work With Frame Work React Js, And I
-          Love To Talk With You About Our Unique.
-        </p>
-        <div className="flex justify-start items-center gap-5 flex-wrap">
-          <Link
-            target="_blank"
-            href={CV}
-            className="border-white border-[1px] rounded-full  px-8 py-2 "
-          >
-            Open CV
-          </Link>
-          <Link
-            href={"#contact"}
-            className="border-none  rounded-full  px-4 py-2 sp"
-          >
-            Let&#39;s Talk{" "}
-          </Link>
-        </div>
-      </div>
-      <Image
-        src={"/mahmoud-removebg.png"}
-        alt="Mahmoud"
-        width={350}
-        height={380}
-        className="bg-cover"
-        loading="eager"
-      />
-    </section>
-  );
+                <p className="max-w-lg font-medium  text-gray-400">
+                    Front-End Developer From Egypt. I Have Rich Experience In
+                    Web Site Development And Building, I Also Work With Frame
+                    Work React Js, And I Love To Talk With You About Our Unique.
+                </p>
+                <div className="flex justify-start items-center gap-5 flex-wrap">
+                    <Link
+                        target="_blank"
+                        href={CV}
+                        className="border-white border-[1px] rounded-full  px-8 py-2 "
+                    >
+                        Open CV
+                    </Link>
+                    <Link
+                        href={"#contact"}
+                        className="border-none  rounded-full  px-4 py-2 sp"
+                    >
+                        Let&#39;s Talk{" "}
+                    </Link>
+                </div>
+            </div>
+            <img
+                src={"/mahmoud-removebg.avif"}
+                alt="Mahmoud"
+                width={350}
+                height={380}
+                className="bg-cover"
+                rel=""
+                fetchPriority="high"
+            />
+        </section>
+    );
 };
 
 export default Hero;
